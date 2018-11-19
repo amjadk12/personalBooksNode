7c58b118334b08f1f4745c49ada2e520f8393a88
Amjad Khan <amjadk12@gmail.com>
2018-11-19 17:28:52 +0530

    Added the authentication, User registration, JWT token generation and loadash related stuff

--- index.js
+++ index.js
@@ -12,22 +12,3 @@ const port = process.env.PORT || 777;
 app.listen(port, () => {
   console.log(`Listing to port ${port}...`);
 });
-
-//module.exports=server;
-
-// module.exports = server;
-// app.get("/", function(req, res) {
-//   res.send("now I can start wring my books" + process.env.NODE_ENV);
-// });
-// app.get("/book1", function(req, res) {
-//   res.send(
-//     `start wring my book - I :
-//     PORT: ${app.get("env")}
-//     Application name: ${config.get("name")}
-//     App Mail: ${config.get("mail.host")}
-//   `
-//   );
-// });
-// app.get("/book2", function(req, res) {
-//   res.send("start wring my book - II");
-// });
--- startup/routes.js
+++ startup/routes.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const books = require("../routes/books");
+const users = require("../routes/users");
 const error = require("../middleware/error");
 
 module.exports = function(app) {
   app.use(express.json());
   app.use("/api/books", books);
+  app.use("/api/users", users);
   app.use(error);
 };
