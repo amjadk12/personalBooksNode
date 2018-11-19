f593485cd7d7398648d316e27e353b0de159d195
Amjad Khan <amjadk12@gmail.com>
2018-11-19 18:41:25 +0530

    Updated the books and added the auth request based on email and password

--- models/book.js
+++ models/book.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Joi = require("joi");
 
 const bookSchema = new mongoose.Schema({
   name: {
@@ -10,10 +11,35 @@ const bookSchema = new mongoose.Schema({
   description: {
     type: String,
     minlength: 10,
-    maxlength: 250
+    maxlength: 255,
+    required: true
+  },
+  fulldetails: {
+    type: String,
+    minlength: 20,
+    maxlength: 2000
   }
 });
 
 const Book = mongoose.model("books", bookSchema);
 
+function validateBook(book) {
+  const schema = {
+    name: Joi.string()
+      .min(2)
+      .max(50)
+      .required(),
+    description: Joi.string()
+      .min(5)
+      .max(255)
+      .required(),
+    fulldetails: Joi.string()
+      .min(20)
+      .max(2000)
+  };
+
+  return Joi.validate(book, schema);
+}
+
+exports.validate = validateBook;
 exports.Book = Book;
--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { Book } = require("../models/book");
+const { Book, validate } = require("../models/book");
 
 router.get("/", async (req, res) => {
   const books = await Book.find();
@@ -8,9 +8,13 @@ router.get("/", async (req, res) => {
 });
 
 router.post("/", async (req, res) => {
+  const { error } = validate(req.body);
+  if (error) return res.status(400).send(error.details[0].message);
+
   let book = new Book({
     name: req.body.name,
-    description: req.body.description
+    description: req.body.description,
+    fulldetails: req.body.fulldetails ? req.body.fulldetails : ""
   });
   book = await book.save();
   res.send(book);
